a6cc34cdd82bc88fe2e0a9599711713e7f069a1a
Mohammad Anas Khan <75521544+anaskhn@users.noreply.github.com>
2024-09-27 04:23:54 +0530

    Add files via upload

---
+++ api-gateway/src/main/java/com/anas/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.anas.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	//client sends req to api gateway -> gateway searches in the eurekaserver the service required and calls it
+}
---
+++ api-gateway/src/test/java/com/anas/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anas.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
